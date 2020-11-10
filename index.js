0c6c8fe29513daa13996e8713693d02efe67d877
RedExtremePT <tomas-250@outlook.com>
2020-11-10 22:46:01 +0000

    Animations and whatnot

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+    $("#0").hide()
+
     $(".navbar a#1").mouseover(function(){
         $("body").css("background-color", "#42c2f5")
         $(this).css("background-color", "#42c2f5")
@@ -11,9 +14,23 @@ $(document).ready(function(){
         $("body").css("background-color", "#ff4d35")
         $(this).css("background-color", "#ff4d35")
     })
+    $(".navbar a#4").mouseover(function(){
+        $("body").css("background-color", "#78ff56")
+        $(this).css("background-color", "#78ff56")
+    })
     $(".navbar a").mouseleave(function(){
         $("body").css("background-color", "white")
         $(".navbar a").css("background-color", "transparent")
 
     })
+
+    $(document).scroll(function(event){
+        var scroll = $(window).scrollTop();
+        if(scroll >= 10){
+            $("#title, #subtitle").fadeOut();
+        } else {
+            $("#title, #subtitle").fadeIn();
+        }
+    })
+
 })
