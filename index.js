22580e5b7bc9e7fe877b78cb42269cbcc2571e8b
RedExtremePT <tomas-250@outlook.com>
2020-11-17 22:47:42 +0000

    Reset ao site

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
 $(document).ready(function(){
 
-    $("#0").hide()
 
-    $(".navbar a#1").mouseover(function(){
-        $("body").css("background-color", "#42c2f5")
-        $(this).css("background-color", "#42c2f5")
-    })
-    $(".navbar a#2").mouseover(function(){
-        $("body").css("background-color", "#810aff")
-        $(this).css("background-color", "#810aff")
-    })
-    $(".navbar a#3").mouseover(function(){
-        $("body").css("background-color", "#ff4d35")
-        $(this).css("background-color", "#ff4d35")
-    })
-    $(".navbar a#4").mouseover(function(){
-        $("body").css("background-color", "#78ff56")
-        $(this).css("background-color", "#78ff56")
-    })
-    $(".navbar a").mouseleave(function(){
-        $("body").css("background-color", "white")
-        $(".navbar a").css("background-color", "transparent")
-
-    })
-
-    $(document).scroll(function(event){
-        var scroll = $(window).scrollTop();
-        if(scroll >= 10){
-            $("#title, #subtitle").fadeOut();
-        } else {
-            $("#title, #subtitle").fadeIn();
-        }
-    })
 
 })
