7075cd2a6211e61ffb8394af0e14b9e5c89407ed
RedExtremePT <tomas-250@outlook.com>
2020-11-21 17:36:43 +0000

    Coisas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
+    $(".registerBox").hide()
 
+    $(".register-link a#register").click(function(){
+        $(".loginBox").fadeOut();
+        $(".flair").css({
+            "transition": "0.7s",
+            "width": "calc(100% + 1050px)"
+        });
+        setTimeout(() => {
+            $(".registerBox").fadeIn();
+        }, 700)
+    })
+    $(".register-link a#login").click(function(){
+        $(".registerBox").fadeOut();
+        $(".flair").css({
+            "transition": "0.7s",
+            "width": "550px"
+        });
+        setTimeout(() => {
+            $(".loginBox").fadeIn();
+        }, 700)
+    })
+
+    $(".close").click(function(){
+        $(".registerBox, .loginBox, .close").fadeOut()
+    })
 
 })
