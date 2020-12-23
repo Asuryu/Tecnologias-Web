5db4b84f802823fbcf4f1d530fada574b83584b5
RedExtremePT <tomas-250@outlook.com>
2020-12-23 16:40:33 +0000

    Yes sir

--- js/index.js
+++ js/index.js
@@ -6,7 +6,19 @@ $(document).ready(function(){
         "width": "0px"
     });
 
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 0){
+            $(".darkner").css({"background-color": "#292929"})
+            $(".navbg").css({"background-color": "#212121"})
+        } else {
+            $(".darkner").css({"background-color": "#111111d5"})
+            $(".navbg").css({"background-color": "transparent"})
+        }
+    })
+
     $(".register-link a#register").click(function(){
+        $(".flair, .loginBox, .registerBox").css({"position": "fixed"})
         $(".loginBox").fadeOut();
         $(".close i").addClass("flip")
         $(".flair").css({
@@ -18,6 +30,7 @@ $(document).ready(function(){
         }, 700)
     })
     $(".register-link a#login").click(function(){
+        $(".flair, .loginBox, .registerBox").css({"position": "fixed"})
         $(".registerBox").fadeOut();
         $(".flair").css({
             "transition": "0.7s",
@@ -29,6 +42,7 @@ $(document).ready(function(){
     })
 
     $(".close").click(function(){
+        $(".flair, .loginBox, .registerBox").css({"position": "fixed"})
         $(".registerBox, .loginBox, .close").fadeOut()
         $(".flair").css({
             "transition": "0.7s",
@@ -37,6 +51,7 @@ $(document).ready(function(){
         $(".log").fadeIn()
     })
     $(".log").click(function(){
+        $(".flair, .loginBox, .registerBox").css({"position": "fixed"})
         $(".log").fadeOut()
         $(".flair").css({
             "transition": "0.7s",
